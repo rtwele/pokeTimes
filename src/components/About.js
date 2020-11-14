ddf29bf329a605ab92b86975653e973479b9886f
rtwele <ryantwele@gmail.com>
2020-11-14 10:34:04 -0500

    react app

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic quae molestiae aut minus perferendis nam dolorem! Expedita quas doloribus quo fuga nesciunt possimus ea, soluta magnam aliquid rerum ad praesentium beatae unde debitis laborum laboriosam consequuntur laudantium amet velit fugiat et hic sapiente explicabo? Natus quidem ex tempora officiis?</p>            
+        </div>
+    )
+}
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto hic quae molestiae aut minus perferendis nam dolorem! Expedita quas doloribus quo fuga nesciunt possimus ea, soluta magnam aliquid rerum ad praesentium beatae unde debitis laborum laboriosam consequuntur laudantium amet velit fugiat et hic sapiente explicabo? Natus quidem ex tempora officiis?</p>            
+        </div>
+    )
+}
+export default Contact
